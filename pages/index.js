b86c11cd2d32c6c6bca4437c44513107e5c6ba16
duhragon <shubhamdhoot1226@gmail.com>
2023-10-21 13:50:40 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import react from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>SpacexX Launch finder</title>
+        <meta name="description" content="SpaceX launch missions." />
+        <meta name="keywords" content="SpaceX, astronomy, space, launch, rocket" />
+        <meta name="author" content="Shubham Dhoot" />
+      </Head>
+    </div>
+  );
+}
